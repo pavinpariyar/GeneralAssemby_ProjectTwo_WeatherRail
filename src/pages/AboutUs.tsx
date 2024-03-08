24cf2d7cb4ccac8bd5486ee040f9a33d6be0f053
Dinul Haque <66052039+dinulhaque@users.noreply.github.com>
2024-03-08 12:14:03 +0000

    added AboutUs.tsx

---
+++ src/pages/AboutUs.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const AboutUs: React.FC = () => {
+    return (
+      <section className="section">
+        <div className="container">
+          <div className="columns is-centered">
+            <div className="column is-half">
+              <div className="card">
+                <div className="card-content">
+                  <h1 className="title has-text-centered">About the Artisans of Code</h1>
+                  <div className="content">
+                    <p>Hello, World-changers! We're Pavin and Dinul—coding enthusiasts by day, digital dream weavers by night. On a whimsical quest to master the wizardry of code, we've embarked on an electrifying adventure that's taken us to the dazzling depths of development. Presenting our second marvel of engineering (but who's keeping score when you're having a blast, right?).</p>
+                    <p>Brace yourself for an exhilarating journey through our technicolor tech stack—a dynamic concoction we've brewed with the essence of React (for that modular magic), a dash of TypeScript (spelling out our code like a grammar guru), and a sprinkle of Bulma's sass to make our CSS dance with elegance and flair.</p>
+                    <p>Envision our digital castle's architecture, where each nook is meticulously mapped. Our 'components' chamber houses an array of mystical trinkets ready to be summoned. The 'pages' hall echoes with stories of yore and adventures yet to come. And down in the 'services' dungeon, our APIs brew and bubble, concocting powerful data elixirs.</p>
+                    <p>Embark on this electrifying escapade with us, and let the navbar be your compass, always there to guide you through this labyrinth of wonders. Cheers to the escapades that await in the curious world of webcraft!</p>
+                    <p>Feel free to journey through the corridors of our creation, and remember—the navbar is your guiding star in this cosmic voyage of code! 🚀🌟</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  };
+  
+  export default AboutUs;
